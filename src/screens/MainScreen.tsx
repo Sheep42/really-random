ab31fbf2e89264de8be4830509bed925148bdea0
Dan Shedd <dan@dshedd.com>
2021-05-07 23:44:24 -0400

    Refactor & style MainScreen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,42 +1,64 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import random from '../api/random';
+import React, { useState } from 'react';
+import { StyleSheet, StatusBar } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { roll } from '../api/random';
 
 const MainScreen = () => {
 
+    const [errorMsg, setErrorMsg] = useState( '' );
+
     const generateRandom = async () => {
 
-        const res = await random.post( '', { 
-            jsonrpc: "2.0",
-            method: "generateIntegers",
-            params: {
-                "apiKey": "load-api-key-here",
-                "n": 3,
-                "min": 1,
-                "max": 6,
-                "replacement": true,
-                "base": 10,
-                "pregeneratedRandomization": null
-            },
-            id: 1154
-        });
-    
-        console.log( res.data );
-    
+        try {
+
+            const res = await roll( 1, 6, 3 );
+
+            return res.data.result.random.data
+        
+        } catch( err ) {
+
+            console.log( err );
+            setErrorMsg( 'Encountered an error, please try again' );
+
+            return [];
+
+        }
+        
     };
 
     // generateRandom();
 
     return (
-        <View>
-            <Text>MainScreen</Text>
-        </View>
+        <SafeAreaView style={ styles.container }>
+            <StatusBar 
+                translucent
+                backgroundColor='transparent' 
+                barStyle='light-content'
+            />
+            <Text h2 style={ styles.title }>Really Random</Text>
+            <Text style={ styles.subtitle }>A true random dice roller</Text>
+        </SafeAreaView>
     );
 
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: '#111111',
+        flex: 1,
+    },
+    title: {
+        textAlign: 'center',
+        marginBottom: 20,
+        marginTop: 20,
+        color: '#ffffff',
+    },
+    subtitle: {
+        fontSize: 15,
+        textAlign: 'center',
+        color: '#ffffff',
+    }
 });
 
 export default MainScreen;
