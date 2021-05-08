0cf0c6a505c3016b8fb5b9b739eeae16af96dd19
Dan Shedd <dan@dshedd.com>
2021-05-08 16:33:18 -0400

    Add all buttons to MainScreen & clean up styles

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { StyleSheet, StatusBar } from 'react-native';
+import { StyleSheet, StatusBar, View } from 'react-native';
 import { Text } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import RollButton from '../components/RollButton';
 import RollList from '../components/RollList';
@@ -8,38 +9,103 @@ import useRandom from '../hooks/useRandom';
 
 const MainScreen = () => {
 
-    const [generateRandom, result, errorMsg] = useRandom();
+    const [generateRandom, clearResult, result, errorMsg] = useRandom();
 
     return (
         <SafeAreaView style={ styles.container }>
-          
             <StatusBar 
                 translucent
                 backgroundColor='transparent' 
                 barStyle='light-content'
             />
-
+                
             <Text h2 style={ styles.title }>Really Random</Text>
             <Text style={ styles.subtitle }>A true random dice roller</Text>
 
-            <RollButton 
-                title="Roll d4"
-                onPress={ () => {
-                        generateRandom( 1, 4, 1 );
-                    } 
-                }
-            />
+            <View style={ styles.btnContainer }>
 
-            <RollList 
-                result={ result }
-            />
- 
-            {
-                ( errorMsg ) ? 
+                <RollButton 
+                    title="d4"
+                    onPress={ () => {
+                            generateRandom( 1, 4, 1 );
+                        } 
+                    }
+                />
+                
+                <RollButton 
+                    title="d6"
+                    onPress={ () => {
+                            generateRandom( 1, 6, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton 
+                    title="d8"
+                    onPress={ () => {
+                            generateRandom( 1, 8, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton 
+                    title="d10"
+                    onPress={ () => {
+                            generateRandom( 1, 10, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton 
+                    title="d12"
+                    onPress={ () => {
+                            generateRandom( 1, 12, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton 
+                    title="d20"
+                    onPress={ () => {
+                            generateRandom( 1, 20, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton 
+                    title="d100"
+                    onPress={ () => {
+                            generateRandom( 1, 100, 1 );
+                        } 
+                    }
+                />
+
+                <RollButton
+                    title="Clear"
+                    onPress={ () => {
+                            
+                            clearResult();
+
+                        }
+                    }
+                />
+
+            </View>           
+
+            <View style={ styles.listContainer }>
+                <Text style={ styles.subtitle }>Rolls</Text>
+        
+                {
+                    ( errorMsg ) ? 
                     <Text style={ styles.error }>{ errorMsg }</Text>
-                : null
-            }
-            
+                    : null
+                }
+
+                <RollList 
+                    result={ result }
+                />
+            </View>
+
         </SafeAreaView>
     );
 
@@ -49,6 +115,16 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#111111',
         flex: 1,
+    },
+    btnContainer:{
+        flex: 1,
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 15,
+    },
+    listContainer: {
+        flex: 2,
     },
     title: {
         textAlign: 'center',
@@ -60,6 +136,7 @@ const styles = StyleSheet.create({
         fontSize: 15,
         textAlign: 'center',
         color: '#ffffff',
+        marginBottom: 30,
     },
     error: {
         padding: 20,
@@ -67,9 +144,6 @@ const styles = StyleSheet.create({
         color: '#f00f00',
 
     },
-    text: {
-        color: '#ffffff',
-    }
 });
 
 export default MainScreen;
