18c350d0fa2d1d8394b08e934fe574a7e8c11a27
Dan Shedd <dan@dshedd.com>
2021-05-08 15:36:27 -0400

    Create useRandom hook

---
+++ src/hooks/useRandom.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { roll } from '../api/random';
+
+export default () => {
+
+    const [errorMsg, setErrorMsg] = useState( '' );
+    const [result, setResult] = useState<Array<Number>>( [] );
+
+    const generateRandom = async ( min: Number, max: Number, amt: Number ) => {
+
+        setErrorMsg( '' );
+
+        try {
+
+            const res = await roll( min, max, amt );
+
+            setResult( [...res.data.result.random.data, ...result] );
+        
+        } catch( err ) {
+
+            console.log( err );
+            setErrorMsg( 'Encountered an error, please try again' );
+
+            setResult( [] );
+
+        }
+        
+    };
+
+    return [ generateRandom, result, errorMsg ] as const;
+
+};
